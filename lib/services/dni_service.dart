e3219277f930f0c0f0fc3d7152fafad618dc86cc
lfcoelloc <lfcoelloc@gmail.com>
2026-01-21 19:56:37 -0500

    Function Province name get and add map of province's names

--- lib/services/dni_service.dart
+++ lib/services/dni_service.dart
@@ -15,16 +15,51 @@ class DniService {
     dni = val;
   }
 
-  bool isThirdCharacterLessThanSix(String dni) {
+  Map provinceMap = {
+    1: 'Azuay',
+    2: 'Bolívar',
+    3: 'Cañar',
+    4: 'Carchi',
+    5: 'Cotopaxi',
+    6: 'Chimborazo',
+    7: 'El Oro',
+    8: 'Esmeraldas',
+    9: 'Guayas',
+    10: 'Imbabura',
+    11: 'Loja',
+    12: 'Los Ríos',
+    13: 'Manabí',
+    14: 'Morona Santiago',
+    15: 'Napo',
+    16: 'Pastaza',
+    17: 'Pichincha',
+    18: 'Tungurahua',
+    19: 'Zamora Chinchipe',
+    20: 'Galápagos',
+    21: 'Sucumbíos',
+    22: 'Orellana',
+    23: 'Santo Domingo de los Tsáchilas',
+    24: 'Santa Elena',
+  };
 
-    
+  String? getProvinciaIfThirdCharLessThanSix(String dni) {
+  if (dni.length != 10) return null;
 
+  // Código provincia
+  final provinciaCodigo = int.tryParse(dni.substring(0, 2));
+  if (provinciaCodigo == null) return null;
 
-    if (dni.length != 10) return false;
+  // Nombre provincia
+  final provinciaNombre = provinceMap[provinciaCodigo];
+  if (provinciaNombre == null) return null;
 
-    final thirdChar = int.tryParse(dni[2]);
-    if (thirdChar == null) return false;
+  // Tercer dígito
+  final thirdChar = int.tryParse(dni[2]);
+  if (thirdChar == null) return null;
 
-    return thirdChar < 6;
-  }
+  if (thirdChar >= 6) return null;
+
+  return provinciaNombre;
+}
+
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -3,15 +3,26 @@ import 'package:test_demo/services/dni_service.dart';
 
 void main() {
   group('validations of input <DNI>', () {
-    test('validate the 3rd character is less than 6', () {
+    test('validate the 3rd character is less than 6 and show province', () {
       // arrange
-      final dniService = DniService('1002003001');
+      final dniService = DniService('1205263411');
+
       // act
-      final result = dniService.isThirdCharacterLessThanSix(
+      final result = dniService.getProvinciaIfThirdCharLessThanSix(
         dniService.getDni(),
       );
+      if (result != null) {
+        print('Provincia válida: $result');
+      } else {
+        print('DNI inválido');
+      }
+
+      // Espera que sea la provincia correspondiente al código 17
+      final expectedProvince = dniService
+          .provinceMap[int.parse(dniService.getDni().substring(0, 2))];
+
       // assert
-      expect(result, true);
+      expect(result, expectedProvince);
     });
   });
 }
